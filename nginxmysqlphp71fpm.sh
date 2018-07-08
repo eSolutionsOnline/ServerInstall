2ad258bfd38c28d388dd109dd306ab9e2e2715ed
Timothy Bannister <timothyjbannister@gmail.com>
2018-07-08 22:50:52 +0100

    7.1 Mcrypt

--- nginxmysqlphp71fpm.sh
+++ nginxmysqlphp71fpm.sh
@@ -12,7 +12,7 @@ apt-add-repository ppa:ondrej/php -y
 apt-get update
 
 echo "Install Some Basic Packages"
-apt-get install -y nginx php7.1-cli php7.1-gd php7.1-dev php7.1-curl php7.1-memcached php7.1-imap php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-soap php7.1-intl php7.1-readline php-xdebug php7.1-fpm
+apt-get install -y nginx php7.1-cli php7.1-gd php7.1-dev php7.1-curl php7.1-memcached php7.1-imap php7.1-mbstring php7.1-xml php7.1-zip php7.1-bcmath php7.1-soap php7.1-intl php7.1-readline php-xdebug php7.1-fpm 7.1-mcrypt
 
 echo "Install Composer"
 curl -sS https://getcomposer.org/installer | php
