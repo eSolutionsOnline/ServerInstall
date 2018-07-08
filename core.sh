8043728672f6f962042fdd152722f9d1d38df4d6
Timothy Bannister <timothyjbannister@gmail.com>
2018-07-08 22:55:23 +0100

    Core

--- core.sh
+++ core.sh
@@ -13,7 +13,7 @@ apt-add-repository ppa:chris-lea/redis-server -y
 apt-get update
 
 echo "Install Some Basic Packages"
- build-essential git libmcrypt4 libpcre3-dev ntp unzip supervisor ufw curl git-core git-flow nodejs redis-server memcached beanstalkd ntpdate
+apt-get install build-essential git libmcrypt4 libpcre3-dev ntp unzip supervisor ufw curl git-core git-flow nodejs redis-server memcached beanstalkd ntpdate
  
 echo "Set My Timezone"
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
